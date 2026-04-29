b134caffcbb454bd7a2997ae5dd365bd28fb2111
huuthang-0355 <vohuuthang3110@gmail.com>
2026-04-29 11:09:50 +0700

    trigger

--- cart/src/main/java/com/yas/cart/CartApplication.java
+++ cart/src/main/java/com/yas/cart/CartApplication.java
@@ -6,12 +6,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication(scanBasePackages = {"com.yas.cart", "com.yas.commonlibrary"})
-@EnableConfigurationProperties({ServiceUrlConfig.class, CorsConfig.class})
+@SpringBootApplication(scanBasePackages = { "com.yas.cart", "com.yas.commonlibrary" })
+@EnableConfigurationProperties({ ServiceUrlConfig.class, CorsConfig.class })
 public class CartApplication {
 
     public static void main(String[] args) {
-        // retest pipeline for cart service
+        // retest pipeline for cart
         SpringApplication.run(CartApplication.class, args);
     }
 }
