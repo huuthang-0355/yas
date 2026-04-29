2a4b72b53086b596b8dc6cde19a65f8d579f1f8e
huuthang-0355 <vohuuthang3110@gmail.com>
2026-04-29 07:25:45 +0700

    add unitest for product service

---
+++ product/src/main/java/com/yas/product/ProductApplication.java
@@ -0,0 +1,18 @@
+package com.yas.product;
+
+import com.yas.commonlibrary.config.CorsConfig;
+import com.yas.commonlibrary.config.ServiceUrlConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication(scanBasePackages = {"com.yas.product", "com.yas.commonlibrary"})
+@EnableConfigurationProperties({ServiceUrlConfig.class, CorsConfig.class})
+public class ProductApplication {
+
+    public static void main(String[] args) {
+        // test for product
+        SpringApplication.run(ProductApplication.class, args);
+    }
+
+}
